65d6262185012bb836362bbe4ccbea787be229ec
lgferreyra <leonarodgferreyra>
2016-10-24 01:00:14 -0300

    lalala

--- js/controles/controles.js
+++ js/controles/controles.js
@@ -1,26 +1,30 @@
 //PIZZERIA
-app.controller("controlInicio", function(){
+app.controller("controlInicio", function($scope, $state){
 
 });
 
-app.controller("controlPizzeria", function(){
+app.controller("controlPizzeria", function($scope){
+
+	function showBack(){
+		return !$state.is("pizzeria.inicio");
+	}
 
 });
 
-app.controller("controlPizzeriaInicio", function(){
+app.controller("controlPizzeriaInicio", function($scope){
 	
 });
 
-app.controller("controlPizzeriaLogin", function(){
+app.controller("controlPizzeriaLogin", function($scope){
 	
 });
 
 
 //INMOBILIARIA
-app.controller("controlInmobiliaria", function(){
+app.controller("controlInmobiliaria", function($scope){
 	
 });
 
-app.controller("controlInmobiliariaInicio", function(){
+app.controller("controlInmobiliariaInicio", function($scope){
 	
 });
